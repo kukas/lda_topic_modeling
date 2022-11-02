0eb0448edbea674ddd25ef6e209023e39369c3ba
Jirka Balhar <balhar.j@gmail.com>
2022-11-02 14:03:34 +0100

    add caching of the preprocessed data

--- lda-data.py
+++ lda-data.py
@@ -9,23 +9,12 @@ from nltk.stem import WordNetLemmatizer, SnowballStemmer
 from nltk.stem.porter import *
 import numpy as np
 import random
+import os
 
 import nltk
 
-nltk.download("wordnet")
-nltk.download("omw-1.4")
-
-
-# Load documents
-
-newsgroups_train = fetch_20newsgroups(subset="train")
-print(len(newsgroups_train.data), " documents loaded.")
-
-print("Example document:")
-print(newsgroups_train.data[0])
-
-
-# Preprocess documents - lemmatization and stemming
+# nltk.download("wordnet")
+# nltk.download("omw-1.4")
 
 
 def lemmatize_stemming(text):
@@ -41,32 +30,50 @@ def preprocess(text):
     return result
 
 
-processed_docs = list(map(preprocess, newsgroups_train.data))
+def load_data():
+    # Load documents
+    newsgroups_train = fetch_20newsgroups(subset="train")
+    print(len(newsgroups_train.data), " documents loaded.")
 
-print("Example document - lemmatized and stemmed:")
-print(processed_docs[0])
+    print("Example document:")
+    print(newsgroups_train.data[0])
+
+    # Preprocess documents - lemmatization and stemming
+    processed_docs = list(map(preprocess, newsgroups_train.data))
+
+    print("Example document - lemmatized and stemmed:")
+    print(processed_docs[0])
+
+    # Construct dictionary
+
+    dictionary = gensim.corpora.Dictionary(processed_docs)
+    dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
+
+    print("Dictionary size: ", len(dictionary))
+
+    # Filter words in documents
+
+    docs = []
+    maxdoclen = 0
+    for doc in processed_docs:
+        docs.append(list(filter(lambda x: x != -1, dictionary.doc2idx(doc))))
+        maxdoclen = max(maxdoclen, len(docs[-1]))
+
+    print("Example document - filtered:")
+    print(docs[0])
+
+    print("Maximum document length:", maxdoclen)
+
+    return docs, dictionary
 
 
-# Construct dictionary
-
-dictionary = gensim.corpora.Dictionary(processed_docs)
-dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
-
-print("Dictionary size: ", len(dictionary))
-
-# Filter words in documents
-
-docs = []
-maxdoclen = 0
-for doc in processed_docs:
-    docs.append(list(filter(lambda x: x != -1, dictionary.doc2idx(doc))))
-    maxdoclen = max(maxdoclen, len(docs[-1]))
-
-print("Example document - filtered:")
-print(docs[0])
-
-print("Maximum document length:", maxdoclen)
-
+# load cached data if available
+cache_path = "data.npy"
+if os.path.isfile(cache_path):
+    docs, dictionary = np.load(cache_path, allow_pickle=True)
+else:
+    docs, dictionary = load_data()
+    np.save(cache_path, [docs, dictionary])
 
 # Set the hyperparameters
 
